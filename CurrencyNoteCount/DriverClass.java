cf89ea5f05cdc88d6eb159f4b74fdf8e093a12ef
MONARCH-CHAUDHARY <118076109+MONARCH-CHAUDHARY@users.noreply.github.com>
2023-04-04 22:07:39 +0530

    Add files via upload

---
+++ CurrencyNoteCount/DriverClass.java
@@ -0,0 +1,33 @@
+package Com.CurrencyNoteCount;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the size of currency denominations");
+		int size=sc.nextInt();
+		
+		
+		int currency[]=new int[size];
+		
+		System.out.println("enter the currency denominations value");		
+		for(int i=0;i<size;i++)
+			currency[i]=sc.nextInt();
+		
+		//System.out.println("Before Sorting "+ Arrays.toString(currency));
+		MergeSort ms=new MergeSort();
+		ms.sort(currency, 0, size-1);
+		//System.out.println("After Sorting "+ Arrays.toString(currency));
+		
+		System.out.println("enter the amount you want to pay");
+		int amount=sc.nextInt();
+		
+		System.out.println("Your payment approach in order to give min no of notes will be");
+		NoteCount nc=new NoteCount();
+		nc.counting(currency,amount);
+		
+	}
+}
